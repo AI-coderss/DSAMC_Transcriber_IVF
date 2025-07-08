6e726fb8dd7936a9dc342c9aa422f29ef850d43f
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2025-07-08 15:18:01 +0300

    clean Backend Code

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-import openai
+from openai import OpenAI
 import os
 import tempfile
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -15,8 +12,7 @@ CORS(app, origins=[
     "https://dsamc-transcriber-ivf-ekthar-center.onrender.com"], supports_credentials=True)
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-client = openai
+client = OpenAI()
 
 def speech_to_text(audio_data_path):
     with open(audio_data_path, "rb") as audio_file:
@@ -54,46 +50,12 @@ def extract_fields(transcript):
     )
     return response.choices[0].message.content
 
-def get_response(user_query):
-    template = """
-    You are a doctor AI assistant. Your main task is to provide medical diagnosis, recommend lab tests and investigations,
-    and prescribe the appropriate drugs based on the user's input reply in English only.
-    User's input: {user_query}
-    Based on the user's input, provide a helpful and detailed response your answers must be always in English only in English regardless of the user's input language.
-    follow the following format :
-    based on the descrbied case
-    The diagnosis : 
-    The recommended lab test and investigation: list them 
-    Drug prescriptions: prescribe the appropriate drugs based on the diagnosis
-    Recommendations to The Doctor: recommend the doctor with regards to case what they supposed to do ?
-    Treatment plan : set the appropriate treatment plan for the doctor including the steps to treat the Patient
-    Please stricly adhere to the above format wherever asked , be more specific and detailed in your answers 
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-    llm = ChatOpenAI(model="gpt-4o")
-    chain = prompt | llm | StrOutputParser()
 
-    return chain.invoke({
-        "user_query": user_query,
-    })
-
-@app.route("/api/generate", methods=["POST"])
-def generate():
-    data = request.get_json()
-    user_query = data.get("input")
-
-    if not user_query:
-        return jsonify({"error": "User input is required"}), 400
-
-    try:
-        response = get_response(user_query)
-        return jsonify({"response": response})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.get("/")
 def index():
     return jsonify({"message": "Welcome to the backend!"})
+
 
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
@@ -144,4 +106,4 @@ def handle_error(e):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
