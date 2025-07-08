14dfc77fcdc6676038eca54190b5e29457ccc905
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2025-07-08 13:12:07 +0300

    CORS Issue Fixed

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 app = Flask(__name__)
-CORS(app, origins=["https://dsamc-transcriber-ivf-ekthar-center.onrender.com"])
+CORS(app, origins=["*"], supports_credentials=True)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 client = openai
 
